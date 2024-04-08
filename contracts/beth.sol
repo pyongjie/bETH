0874a3abd1cf5618d402754151446d6b80f6b91c
pyongjie <pyjavatar@gmail.com>
2024-04-09 01:19:31 +0800

    Implement createBet function

--- contracts/beth.sol
+++ contracts/beth.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract Beth {
+    
     struct bet {
-        uint256 betId;
+        //uint256 betId;
         string betName;
         string side1Description;
         string side2Description;
@@ -12,19 +13,47 @@ contract Beth {
         bool completed;
         bool result;
         address betInitiator;
+        uint8 currentParticipantsCount;
         mapping(address => uint256) side1Bets;
         mapping(address => uint256) side2Bets;
-        uint8 currentParticipantsCount;
     }
 
-    bet[] Bets_Array;
+    uint256 public numBets = 0;
+    mapping(uint256 => bet) bets;
+    
     uint8 minParticipant;
     uint8 commissionFeeBetCreator;
     uint8 commissionFeeDev;
     uint8 transactionFee;
     uint8[] groups;
 
-    
+    //function to create a new bet
+    function createBet(
+        string betName,
+        string side1Description,
+        string side2Description,
+        uint256 minBet,
+        uint256 openingDate,
+        uint256 closingDate
+    ) public payable returns(uint256) {
 
+        //new bet object
+        bet memory newBet = bet(
+            betName,
+            side1Description,
+            side2Description,
+            minBet,
+            openingDate,
+            closingDate,
+            false,
+            false,
+            msg.sender,
+            0
+        );
 
+        uint256 newBetId = numBets++;
+        bets[newBetId] = newBet;
+        return newBetId;
+    }
+ 
 }
