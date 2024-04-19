c4bbe27ec9ea73c42ebe137f60fc51dfe41b7076
chewsday <tyler.wangy@u.nus.edu>
2024-04-20 03:13:12 +0800

    Removed minParticipants; changed wording of address not in group error; added params to deployment script changed truffle config version to v0.8.0

--- contracts/beth.sol
+++ contracts/beth.sol
@@ -2,14 +2,16 @@ pragma solidity ^0.8.0;
 import "./ABDKMath64x64.sol";
 
 contract Beth {
-    uint8 minParticipant;
     uint8 commissionFeeBetCreator;
     uint8 commissionFeeDev;
     uint8 transactionFee;
     address admin;
 
-    constructor(uint8 minParticipantNo, uint8 betCreatorFee, uint8 devFee, uint8 txFee) public payable {
-        minParticipant = minParticipantNo;
+    constructor(
+        uint8 betCreatorFee,
+        uint8 devFee,
+        uint8 txFee
+    ) public payable {
         commissionFeeBetCreator = betCreatorFee;
         commissionFeeDev = devFee;
         transactionFee = txFee;
@@ -32,7 +34,7 @@ contract Beth {
         uint256 stakeSide1Bet;
         uint256 stakeSide2Bet;
         address[] side1BetsAddress;
-        address[] side2BetsAddress; 
+        address[] side2BetsAddress;
         mapping(address => uint256) side1Bets;
         mapping(address => uint256) side2Bets;
     }
@@ -41,7 +43,7 @@ contract Beth {
     mapping(uint256 => bet) bets;
     uint256 public numGroups = 0;
     mapping(uint256 => address[]) groups;
-    uint256 constant DELAY = 1 minutes; // This is 24 hours but for demonstration we use 1 min
+    uint256 constant DELAY = 1440 minutes; // 24 hours
     uint256 constant averageGasLimit = 30000000;
 
     event PayoutDelayed(uint256 executionTime);
@@ -52,12 +54,16 @@ contract Beth {
         _;
     }
 
-    modifier approvedAddresses(uint256 groupId, uint256 betId, address addr) {
+    modifier approvedAddresses(
+        uint256 groupId,
+        uint256 betId,
+        address addr
+    ) {
         bool check = false;
-        
+
         // check if bet group is public
         if (groupId == 0) {
-            check = true;    
+            check = true;
         } else {
             // check if address is in approved list of addresses for private group
             address[] memory arr = groups[groupId];
@@ -68,13 +74,18 @@ contract Beth {
             }
         }
 
-        require(check == true, "Current address is not allowed to place bet");
+        require(
+            check == true,
+            "Caller is not approved to place bet in this group"
+        );
         _;
     }
 
-
     modifier onlyAfter(uint256 time) {
-        require(block.timestamp >= time, "Payout can only be called after 24 hours");
+        require(
+            block.timestamp >= time,
+            "Payout can only be called after 24 hours"
+        );
         _;
     }
 
@@ -100,11 +111,11 @@ contract Beth {
         uint256 openingDate,
         uint256 closingDate,
         uint256 groupId
-    ) public existingGroup(groupId) returns(uint256) {
+    ) public existingGroup(groupId) returns (uint256) {
         //new bet object
         uint256 newBetId = numBets++;
         bet storage newBet = bets[newBetId];
-        
+
         //initialize relevant variables in new bet object
         newBet.betName = betName;
         newBet.side1Description = side1Description;
@@ -128,13 +139,22 @@ contract Beth {
     function placeBet(
         uint256 betId,
         bool betSide
-    ) public payable 
-      validBet(betId)
-      approvedAddresses(bets[betId].groupId, betId, msg.sender) 
-    {   
-        require(msg.value >= bets[betId].minBet, "Bet amount is less than minimum bet");
-        require(block.timestamp >= bets[betId].openingDate && block.timestamp <= bets[betId].closingDate, "Bet not placed within betting dates");
-        
+    )
+        public
+        payable
+        validBet(betId)
+        approvedAddresses(bets[betId].groupId, betId, msg.sender)
+    {
+        require(
+            msg.value >= bets[betId].minBet,
+            "Bet amount is less than minimum bet"
+        );
+        require(
+            block.timestamp >= bets[betId].openingDate &&
+                block.timestamp <= bets[betId].closingDate,
+            "Bet not placed within betting dates"
+        );
+
         if (betSide) {
             //update bet object and side 1 bet variables
             uint256 curSide1Amt = bets[betId].stakeSide1Bet;
@@ -156,12 +176,16 @@ contract Beth {
     }
 
     // function to view bet side 1 name
-    function viewBetSide1Name(uint256 betId) public view returns (string memory) {
+    function viewBetSide1Name(
+        uint256 betId
+    ) public view returns (string memory) {
         return bets[betId].side1Description;
     }
 
     // function to view bet side 2 name
-    function viewBetSide2Name(uint256 betId) public view returns (string memory) {
+    function viewBetSide2Name(
+        uint256 betId
+    ) public view returns (string memory) {
         return bets[betId].side2Description;
     }
 
@@ -186,18 +210,21 @@ contract Beth {
     }
 
     // function to view the current odds
-    function viewCurrentOdds(uint256 betId) public view returns(int128) {
+    function viewCurrentOdds(uint256 betId) public view returns (int128) {
         // For the case when the denominator is 0
         if (bets[betId].stakeSide2Bet == 0) {
             return 0;
         }
-        return ABDKMath64x64.divu(bets[betId].stakeSide1Bet, bets[betId].stakeSide2Bet);
+        return
+            ABDKMath64x64.divu(
+                bets[betId].stakeSide1Bet,
+                bets[betId].stakeSide2Bet
+            );
     }
-
 
     // Group Functions
     //function to create a group for private bets
-    function createGroup(address[] memory arr) public returns(uint256) {
+    function createGroup(address[] memory arr) public returns (uint256) {
         numGroups++;
         uint256 newGroupId = numGroups;
         groups[newGroupId] = arr;
@@ -205,24 +232,34 @@ contract Beth {
     }
 
     //function to view group for private bets
-    function viewGroup(uint256 groupId) public view returns(address[] memory) {
+    function viewGroup(uint256 groupId) public view returns (address[] memory) {
         address[] memory arr = groups[groupId];
         return arr;
     }
 
-
     // Admin Functions
     // wrapper function for admin to pay betters with 24 hours delay
-    function executePayout(uint256 betId, bool result) public adminOnly() validBet(betId) {
-        require(block.timestamp >= bets[betId].closingDate, "Payout can only be executed after closing date");
+    function executePayout(
+        uint256 betId,
+        bool result
+    ) public adminOnly validBet(betId) {
+        require(
+            block.timestamp >= bets[betId].closingDate,
+            "Payout can only be executed after closing date"
+        );
         bets[betId].result = result;
         bets[betId].payoutDate = block.timestamp + DELAY;
         emit PayoutDelayed(bets[betId].payoutDate);
     }
 
     // function to payout betters
-    function payout(uint256 betId) public adminOnly() onlyAfter(bets[betId].payoutDate) {
-        require(bets[betId].closingDate != 0, "Admin has not set results, so payout date is not set");
+    function payout(
+        uint256 betId
+    ) public adminOnly onlyAfter(bets[betId].payoutDate) {
+        require(
+            bets[betId].closingDate != 0,
+            "Admin has not set results, so payout date is not set"
+        );
         address[] memory winnerLs;
 
         if (bets[betId].result) {
@@ -231,18 +268,24 @@ contract Beth {
             winnerLs = bets[betId].side2BetsAddress;
         }
 
-        uint256 totalPrizePool = bets[betId].stakeSide1Bet + bets[betId].stakeSide2Bet;
+        uint256 totalPrizePool = bets[betId].stakeSide1Bet +
+            bets[betId].stakeSide2Bet;
         uint256 txfee = tx.gasprice * averageGasLimit;
-        uint256 payoutWinners = ((100 - commissionFeeBetCreator - commissionFeeDev) * totalPrizePool / 100)  - txfee * (winnerLs.length + 2);
+        uint256 payoutWinners = (((100 -
+            commissionFeeBetCreator -
+            commissionFeeDev) * totalPrizePool) / 100) -
+            txfee *
+            (winnerLs.length + 2);
 
         //Pay bet initiator
         address payable betCreator = payable(bets[betId].betCreator);
-        uint256 betCreatorPayment = commissionFeeBetCreator * totalPrizePool / 100;
+        uint256 betCreatorPayment = (commissionFeeBetCreator * totalPrizePool) /
+            100;
         betCreator.transfer(betCreatorPayment);
 
         // Pay developers
         address payable dev = payable(admin);
-        dev.transfer(commissionFeeDev * totalPrizePool / 100 );
+        dev.transfer((commissionFeeDev * totalPrizePool) / 100);
 
         // Iterate through winners list to pay each winner depending on their stake
         for (uint i = 0; i < winnerLs.length; i++) {
@@ -250,9 +293,13 @@ contract Beth {
             uint256 payoutPerPerson = 0;
 
             if (bets[betId].result) {
-                payoutPerPerson = bets[betId].side1Bets[recipient] * payoutWinners / bets[betId].stakeSide1Bet ;
+                payoutPerPerson =
+                    (bets[betId].side1Bets[recipient] * payoutWinners) /
+                    bets[betId].stakeSide1Bet;
             } else {
-                payoutPerPerson = bets[betId].side2Bets[recipient] * payoutWinners / bets[betId].stakeSide2Bet ;
+                payoutPerPerson =
+                    (bets[betId].side2Bets[recipient] * payoutWinners) /
+                    bets[betId].stakeSide2Bet;
             }
 
             recipient.transfer(payoutPerPerson);
@@ -264,16 +311,18 @@ contract Beth {
     }
 
     // function for admin to edit result of the bet
-    function changeResult(uint256 betId) public adminOnly() validBet(betId) returns (bool) {
+    function changeResult(
+        uint256 betId
+    ) public adminOnly validBet(betId) returns (bool) {
         bets[betId].result = !bets[betId].result;
         return !bets[betId].result;
     }
 
-    function getCurrentTimestamp() public view returns(uint256) {
+    function getCurrentTimestamp() public view returns (uint256) {
         return block.timestamp;
     }
 
-    function checkGasPrice() public view returns(uint256) {
+    function checkGasPrice() public view returns (uint256) {
         return tx.gasprice;
     }
 }
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,7 +1,7 @@
 const Beth = artifacts.require("Beth");
 
 module.exports = (deployer, network, accounts) => {
-    deployer.deploy(Beth).then(function() {
-        return deployer.deploy(Beth);
-    });
+  deployer.deploy(Beth, 1, 1, 1).then(function () {
+    return deployer.deploy(Beth);
+  });
 };
